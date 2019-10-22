5e0930bff14bff0877a7a81420b73512dbb0b666
ugeh stanley <lildween1998@gmail.com>
2019-10-22 18:06:49 +0100

    ADDED ALL FOLDERS

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// importing from flutter 
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+
+//we have to define a 'main' function to run
+void main(){
+//create a  new text widget to show text on the screen//
+runApp(App());
+}
